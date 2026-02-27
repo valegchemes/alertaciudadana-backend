8a8c1cd1dda598a0104885acab401adb5e9b5e1c
valegchemes <valegchemes@gmail.com>
2026-02-27 17:15:30 -0300

    Temporarily remove role guard for testing

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,16 +1,11 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { AuthGuard } from '@nestjs/passport';
-import { Roles } from '../auth/roles.decorator';
-import { RolesGuard } from '../auth/roles.guard';
 import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(AuthGuard('jwt'), RolesGuard)
-  @Roles('ADMIN')
   @Get()
   findAll() {
     return this.usersService.findAll();
