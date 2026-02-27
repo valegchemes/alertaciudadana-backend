3058d0eca221b58a2129c6e1d0476157451a6ba5
valegchemes <valegchemes@gmail.com>
2026-02-27 17:29:38 -0300

    Fix promote endpoint location

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Roles } from '../auth/roles.decorator';
@@ -19,5 +19,12 @@ export class UsersController {
   @Post()
   create(@Body() userData: Partial<User>) {
     return this.usersService.create(userData);
+  }
+
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('ADMIN')
+  @Patch(':id/promote')
+  promote(@Param('id') id: string) {
+    return this.usersService.promoteToAdmin(+id);
   }
 }
