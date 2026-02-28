5ae2fa635f5ac79c201dcde3c0c19777a75b0ae2
valegchemes <valegchemes@gmail.com>
2026-02-28 04:17:38 -0300

    Clean initial migration for user and report

--- backend/src/migrations/1772262939348-Init.ts
+++ backend/src/migrations/1772262939348-Init.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1772228185274 implements MigrationInterface {
-    name = 'Init1772228185274'
+export class Init1772262939348 implements MigrationInterface {
+    name = 'Init1772262939348'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" character varying NOT NULL DEFAULT 'USER', "firstName" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
