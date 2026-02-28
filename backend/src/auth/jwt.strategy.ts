d1e0df96160f20bec83fc4f83ab11dc85c1a9a9e
valegchemes <valegchemes@gmail.com>
2026-02-28 06:07:51 -0300

    Fix JwtStrategy typing issue

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'supersecretkey',
+      secretOrKey: configService.get<string>('JWT_SECRET') || 'supersecretkey',
     });
   }
 
